5f1129e193f02c7c58fcd51070d01dc4f789c546
Rik Ghosh <rikghosh487@gmail.com>
2021-05-26 19:20:25 -0500

    Modularized Scatterplot generation

--- main.py
+++ main.py
@@ -3,6 +3,36 @@ import pandas as pd
 import numpy as np
 from scipy.stats import gaussian_kde
 
+BG_COLOR = 'rgb(50, 50, 50)'
+OPACITY = 0.8
+MARKER_SIZE = 4
+
+# Assigns color based on Kernel Density Evaluation
+def get_color_kde(dataframe):
+    x = np.asarray(dataframe['pmra'])
+    y = np.asarray(dataframe['pmdec'])
+    xy = np.vstack([x, y])
+    color = gaussian_kde(xy)(xy)
+    dataframe['color'] = color
+    return dataframe
+
+def get_scatterplot(dataframe, paramx, paramy, dimensions, title='Scatterplot',
+                    sequence='magma', display=False, reverse=False):
+    if(display):
+        fig = px.scatter(data_frame=dataframe, x=paramx['name'], y=paramy['name'], opacity=OPACITY,
+                        color='color', title=title, color_continuous_scale=sequence,
+                        labels={
+                            paramx['name']: paramx['axis'],
+                            paramy['name']: paramy['axis']
+                        })
+        fig.update_traces(marker=dict(size=MARKER_SIZE))
+        if reverse:
+            fig.update_layout(height=dimensions, width=dimensions, autosize=False, plot_bgcolor=BG_COLOR,
+                            yaxis=dict(autorange='reversed'))
+        else:
+            fig.update_layout(height=dimensions, width=dimensions, autosize=False, plot_bgcolor=BG_COLOR)
+        fig.show()
+
 def main():
     # reading file
     df = pd.read_csv('result.csv')
@@ -12,11 +42,7 @@ def main():
     df = df[df['pmdec_error'] < 1]
 
     # KDE color determination
-    x = np.asarray(df['pmra'])
-    y = np.asarray(df['pmdec'])
-    xy = np.vstack([x, y])
-    color = gaussian_kde(xy)(xy)
-    df['color'] = color
+    df = get_color_kde(dataframe=df)
 
     # hardcoded Data Reductions
     df = df[df['pmra'] <= 40]
@@ -24,18 +50,10 @@ def main():
     df = df[df['pmdec'] <= 40]
     df = df[df['pmdec'] >= -40]
 
-    '''
-    # Scatter Plot of Denisty Graph
-    fig1 = px.scatter(df, x='pmra', y='pmdec', opacity=0.8, color='color', title='Density Graph',
-                    color_continuous_scale='tealgrn',
-                    labels={
-                        'pmra': '\u03bc \u03b1 (mas/year)',
-                        'pmdec': '\u03bc \u03b4 (mas/year)'
-                    })
-    fig1.update_traces(marker=dict(size=4))
-    fig1.update_layout(height=550, width=550, autosize=False, plot_bgcolor='rgb(50,50,50)')
-    fig1.show()
-    # '''
+    # produces scatterplot for raw data with hard-coded data reductions
+    get_scatterplot(dataframe=df, paramx=dict(name='pmra', axis='\u03bc \u03b1 (mas/year)'),
+                    paramy=dict(name='pmdec', axis='\u03bc \u03b4 (mas/year)'), dimensions=550,
+                    title='Vector Point Diagram (Density Graph)', sequence='tealgrn', display=True)
 
     # parallax stat data
     meanp = np.mean(df['parallax'])
@@ -67,32 +85,17 @@ def main():
     fig4.show()
     # '''
 
-    # '''
-    # VPD Scatter plot after adjusting for parallax
-    fig5 = px.scatter(df, x='pmra', y='pmdec', opacity=0.8, color='color',
-                    title='Parallax Adjusted Density Graph',
-                    color_continuous_scale='tealgrn',
-                    labels={
-                        'pmra': '\u03bc \u03b1 (mas/year)',
-                        'pmdec': '\u03bc \u03b4 (mas/year)'
-                    })
-    fig5.update_traces(marker=dict(size=4))
-    fig5.update_layout(height=550, width=550, autosize=False, plot_bgcolor='rgb(50,50,50)')
-    fig5.show()
-    # '''
+    get_scatterplot(dataframe=df, paramx=dict(name='pmra', axis='\u03bc \u03b1 (mas/year)'),
+                    paramy=dict(name='pmdec', axis='\u03bc \u03b4 (mas/year)'), dimensions=550,
+                    title='Parallax Adjusted VPD Density Graph', sequence='tealgrn', display=False)
 
-    print(len(df))
+    get_scatterplot(dataframe=df, paramx=dict(name='ra', axis='\u03b1 (deg)'),
+                    paramy=dict(name='dec', axis='\u03b4 (deg)'), dimensions=600,
+                    title='Structure of Open Cluster (based on ICRS)', sequence='tealgrn', display=False)
 
-    fig6 = px.scatter(df, x='bp_rp', y='phot_g_mean_mag', opacity=0.8, color='color',
-                      title='Color Magnitude Diagram', color_continuous_scale='tealgrn',
-                      labels={
-                          'bp_rp': 'BP - RP (mag)',
-                          'phot_g_mean_mag': 'G (mag)'
-                      })
-    fig6.update_traces(marker=dict(size=4))
-    fig6.update_layout(height=550, width=550, autosize=False, plot_bgcolor='rgb(50,50,50)',
-                       yaxis=dict(autorange='reversed'))
-    fig6.show()
+    get_scatterplot(dataframe=df, paramx=dict(name='bp_rp', axis='BP - RP (mag)'),
+                    paramy=dict(name='phot_g_mean_mag', axis='G (mag)'), dimensions=700,
+                    title='Color Magnitude Diagram',sequence='tealgrn', display=True, reverse=True)
 
 if __name__ == '__main__':
     main()
