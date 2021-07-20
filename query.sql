69f533f81d2032296bbd12aa2b640e1d64f24d05
Rik Ghosh <rikghosh487@gmail.com>
2021-07-20 17:19:45 -0500

    Updated SQL with increased search span

--- query.sql
+++ query.sql
@@ -1,12 +1,37 @@
+/* 
+	@author: Rik Ghosh
+	@copyright: Copyright 2021, University of Texas at Austin
+	@credits: Soham Saha, Katherine Clark, Mihir Suvarna
+	@license: MIT
+	@version: 1.0.3
+	@maintainer: Rik Ghosh
+	@email: rikghosh487@gmail.com
+	@status: production
+--*/
+
 SELECT
-gaia_source.source_id, gaia_source.ra, gaia_source.ra_error, gaia_source.dec, gaia_source.dec_error,
-gaia_source.parallax, gaia_source.parallax_error, gaia_source.pm, gaia_source.pmra, gaia_source.pmra_error,
-gaia_source.pmdec, gaia_source.pmdec_error, gaia_source.parallax_pmra_corr, gaia_source.parallax_pmdec_corr,
-gaia_source.pmra_pmdec_corr, gaia_source.phot_g_mean_mag, gaia_source.phot_bp_mean_mag,
-gaia_source.phot_rp_mean_mag, gaia_source.bp_rp
-FROM gaiaedr3.gaia_source 
+gaia_source.source_id, -- datatype: long
+gaia_source.ra, -- unit: deg, datatype: double
+gaia_source.ra_error, -- unit: mas, datatype: float
+gaia_source.dec, -- unit: deg, datatype: double
+gaia_source.dec_error, -- unit: mas, datatype: float
+gaia_source.parallax, -- unit: mas, datatype: double
+gaia_source.parallax_error, -- unit: mas, datatype: float
+gaia_source.pm, -- unit: mas/yr, datatype: float
+gaia_source.pmra, -- unit: mas/yr, datatype: double
+gaia_source.pmra_error, -- unit: mas/yr, datatype: float
+gaia_source.pmdec, -- unit: mas/yr, datatype: double
+gaia_source.pmdec_error, -- unit: mas/yr, datatype: float
+gaia_source.parallax_pmra_corr, -- datatype: float
+gaia_source.parallax_pmdec_corr, -- datatype: float
+gaia_source.pmra_pmdec_corr, -- datatype: float
+gaia_source.phot_g_mean_mag, -- unit: mag, datatype: float
+gaia_source.bp_rp, -- unit: mag, datatype: float
+gaia_source.dr2_radial_velocity, -- unit: km/s, datatype: float
+gaia_source.dr2_radial_velocity_error -- unit: km/s, datatype: float
+FROM gaiaedr3.gaia_source
 WHERE 
 CONTAINS(
 	POINT('ICRS',gaiaedr3.gaia_source.ra,gaiaedr3.gaia_source.dec),
-	CIRCLE('ICRS', 345.67348, 59.55911, 0.1)
+	CIRCLE('ICRS', 345.67348, 59.55911, 0.16666666666667)
 )=1  AND  (gaiaedr3.gaia_source.parallax_error<=0.999999)
